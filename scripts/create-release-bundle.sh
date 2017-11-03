82e9cca1cd04bf15d06f511fac68dee6c6f4dcae
Joze Rihtarsic <jrihtarsic1@gmail.com>
2017-11-03 11:38:59 +0100

    - SEDLogger bugfix - change html width, align with syle properties

--- scripts/create-release-bundle.sh
+++ scripts/create-release-bundle.sh
@@ -1,6 +1,24 @@
 #/!bin/sh
 
+# laurentius version
+VERSION="2.0-SNAPSHOT"
+# releases folder 
 LAU_RELEASE_FOLDER="releases"
+# release zip file name
+ZIP_FILENAME="Laurentius-$VERSION-$(date +%Y%m%d_%H%M)"
+
+ZIP_TEST="Laurentius-test"
+FOLDER_DEPLOY="test-deploy"
+
+
+AS_WILDFLY_10_1="wildfly-10.1"
+AS_WILDFLY_11_0="wildfly-11.0"
+AS_JBOSS_EAP_7_0="jboss-eap-7.0"
+
+AS_ARRAY=($AS_WILDFLY_10_1 $AS_WILDFLY_11_0 $AS_JBOSS_EAP_7_0)
+
+
+# 
 LAU_PROJECT=$1
 if [ "x$LAU_PROJECT" = "x" ]; then
 	LAU_PROJECT="../"
@@ -10,39 +28,52 @@ if [ ! -d "$LAU_RELEASE_FOLDER" ]; then
 	mkdir $LAU_RELEASE_FOLDER
 fi
 
-
-VERSION="2.0-SNAPSHOT"
-ZIP_FILENAME="Laurentius-$VERSION-$(date +%Y%m%d_%H%M)"
-ZIP_TEST="Laurentius-test"
-FOLDER_DEPLOY="test-deploy"
-
-
+# clear target folders 
 rm -rf $LAU_RELEASE_FOLDER/$ZIP_FILENAME
 rm -rf $FOLDER_DEPLOY
 
 
-mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME"
-mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules"
-mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments"
-mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
+mkdir    "$LAU_RELEASE_FOLDER/$ZIP_FILENAME"
+mkdir -p "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/"
+mkdir    "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments"
+for asItem in ${AS_ARRAY[*]}
+do
+	mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+	mkdir "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem/config"
+done
+
+# create test deploy folder
 mkdir $FOLDER_DEPLOY
+#--------------------------------------------------------------------------------------
+# COPY LIBRARIES / MODULES
 # si.laurentius module libralies
-cp "$LAU_PROJECT/Laurentius-libs/Laurentius-msh-xsd/target/Laurentius-msh-xsd-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/" 	
-cp "$LAU_PROJECT/Laurentius-libs/Laurentius-wsdl/target/Laurentius-wsdl-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
-cp "$LAU_PROJECT/Laurentius-libs/Laurentius-commons/target/Laurentius-commons-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
-cp "$LAU_PROJECT/Laurentius-libs/Laurentius-lce/target/Laurentius-lce-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
-cp "$LAU_PROJECT/Laurentius-libs/Laurentius-plugin-interfaces/target/Laurentius-plugin-interfaces-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
+cp "$LAU_PROJECT/Laurentius-libs/Laurentius-msh-xsd/target/Laurentius-msh-xsd-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/" 	
+cp "$LAU_PROJECT/Laurentius-libs/Laurentius-wsdl/target/Laurentius-wsdl-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/"
+cp "$LAU_PROJECT/Laurentius-libs/Laurentius-commons/target/Laurentius-commons-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/"
+cp "$LAU_PROJECT/Laurentius-libs/Laurentius-lce/target/Laurentius-lce-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/"
+cp "$LAU_PROJECT/Laurentius-libs/Laurentius-plugin-interfaces/target/Laurentius-plugin-interfaces-$VERSION.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si/laurentius/main/"
 
-cp "$LAU_PROJECT/scripts/wildfly-10.1/modules/si.laurentius.module.xml" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
-sed -i -- "s/VERSION/$VERSION/g" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/si.laurentius.module.xml"
-cp "$LAU_PROJECT/scripts/wildfly-10.1/modules/org.apache.ws.securitymodule.xml" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
-cp -r "$LAU_PROJECT/scripts/wildfly-10.1/modules/org"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
+#create module descriptions for application servers 
+for asItem in ${AS_ARRAY[*]}
+do
+	# copy module descriptors for application servers
+	cp -r "$LAU_PROJECT/scripts/install/$asItem/modules" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem/"
+	# set library versions for si.laureius module
+	sed -i -- "s/VERSION/$VERSION/g" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem/modules/si.laurentius.module.xml"
+done
 
+# copy common libraries (pdfbox, primefaces)
+cp -r "$LAU_PROJECT/scripts/install/modules/org" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/modules/"
+
+
+#--------------------------------------------------------------------------------------
+# COPY APPLICATION MODULES
 # application modules
 cp "$LAU_PROJECT/Laurentius-dao/target/Laurentius-dao.jar" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
 cp "$LAU_PROJECT/Laurentius-msh/Laurentius-msh-ear/target/Laurentius-msh.ear" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
 cp "$LAU_PROJECT/Laurentius-app/Laurentius-ws/target/laurentius-ws.war"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
 cp "$LAU_PROJECT/Laurentius-app/Laurentius-web/target/laurentius-web.war"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
+#--------------------------------------------------------------------------------------
 # application plugins
 cp "$LAU_PROJECT/Laurentius-plugins/Laurentius-zpp-plugin/target/plugin-zpp.war"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
 cp "$LAU_PROJECT/Laurentius-plugins/Laurentius-basic-plugin/target/plugin-basic.war"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
@@ -50,23 +81,35 @@ cp "$LAU_PROJECT/Laurentius-plugins/Laurentius-test-case-plugin/target/plugin-te
 cp "$LAU_PROJECT/Laurentius-examples/example-web-plugin/target/example-web-plugin.war" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/deployments/"
 
 
-# configuration file
-cp -r "$LAU_PROJECT/scripts/wildfly-10.1/config"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
-# deploy script
-cp "$LAU_PROJECT/scripts/wildfly-10.1/deploy-laurentius.sh" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
-sed -i -- "s/VERSION/$VERSION/g" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1/deploy-laurentius.sh"
+#--------------------------------------------------------------------------------------
+# SET APPLICATION SETTINGS AND DEMO DEPLOY/START SCRIPT
 
-cp "$LAU_PROJECT/scripts/wildfly-10.1/laurentius-demo.sh" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
+for asItem in ${AS_ARRAY[*]}
+do
+	# copy application server config script
+	cp -r "$LAU_PROJECT/scripts/install/$asItem/config"  "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+	# copy deploy script
+	cp "$LAU_PROJECT/scripts/install/$asItem/deploy-laurentius.sh" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+	cp "$LAU_PROJECT/scripts/install/$asItem/deploy-laurentius.bat" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+	
+	cp "$LAU_PROJECT/scripts/install/$asItem/laurentius-demo.sh" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+	cp "$LAU_PROJECT/scripts/install/$asItem/laurentius-demo.bat" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/$asItem"
+done
 
-cp "$LAU_PROJECT/scripts/wildfly-10.1/deploy-laurentius.bat" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
-sed -i -- "s/VERSION/$VERSION/g" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1/deploy-laurentius.bat"
 
-cp "$LAU_PROJECT/scripts/wildfly-10.1/laurentius-demo.bat" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/wildfly-10.1"
-
+#--------------------------------------------------------------------------------------
+# COPY INIT HOME DATA  
 # init data:
-cp -r "$LAU_PROJECT/scripts/laurentius-demo" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/laurentius-home"
+cp -r "$LAU_PROJECT/scripts/install/laurentius-home" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME/laurentius-home"
 
-zip -r "$LAU_RELEASE_FOLDER/$ZIP_FILENAME.zip" "$LAU_RELEASE_FOLDER/$ZIP_FILENAME"
+#--------------------------------------------------------------------------------------
+# CREATE_BOUNDLE   
+cd $LAU_RELEASE_FOLDER
+zip -r "$ZIP_FILENAME.zip" $ZIP_FILENAME
+cd ..
+
+#--------------------------------------------------------------------------------------
+# Prepare for test   
 # move "boudle folder to test folder fo test deploy"
 mv $LAU_RELEASE_FOLDER/$ZIP_FILENAME $FOLDER_DEPLOY/$ZIP_TEST
 
---
+++ scripts/install/wildfly-10.1/deploy-laurentius.sh
@@ -0,0 +1,140 @@
+#!/bin/sh
+
+# set fixed parameters
+
+#WILDFLY_HOME=
+#LAU_BUNDLE=
+#LAU_HOME=
+#INIT=
+APPL_SERVER="wildfly-10.1"
+
+while [ "$#" -gt 0 ]
+do
+key="$1"
+
+  case $key in
+    -w|--wildfly)
+      WILDFLY_HOME="$2"
+      shift # past argument
+    ;;
+      -l|--laurentius.home)
+      LAU_HOME="$2"
+      shift # past argument
+    ;;
+      -b|--bundle)
+      LAU_BUNDLE="$2"
+      shift # past argument
+    ;;
+    --init)
+      INIT="TRUE"
+    ;;
+    *)
+      # unknown option
+    ;;
+  esac
+  shift # past argument or value
+done
+
+
+if [ "x$WILDFLY_HOME" = "x" ]; then
+	WILDFLY_HOME=$1
+fi
+
+if [ "x$LAU_BUNDLE" = "x" ]; then
+	LAU_BUNDLE=$2
+fi
+
+
+if [ "x$LAU_HOME" = "x" ]; then
+	LAU_HOME=$3
+fi
+
+quit () {
+	echo "\nUsage:\n"
+	echo "deploy-led.bat --init -b [LAU_BUNDLE] -w [WILDFLY_HOME] -l [LAU_HOME]\n"
+	echo "  --init  initialize laurentius.home and wildfly properties. "
+	echo "  -w   WILDFLY_HOME -  path jboss home: ex.: c:\\temp\\$APPL_SERVER.0.Final\\."
+	echo "  -b   LAU_BUNDLE   - path to unziped Laurentius bundle if not given parent script folder is setted."
+	echo "  -l   LAU_HOME     - path tom application home folder  (laurentius.home) if is not given and --init is setted than '[WILDFLY_HOME]\standalone\data\' is setted.	"
+        exit
+}
+
+if [ "x$WILDFLY_HOME" = "x" ]; then
+	quit;
+fi
+
+if [ ! -d "$WILDFLY_HOME" ]; then
+	echo "WILDFLY_HOME folder not exists! Check parameters!"
+	quit;
+fi
+
+
+if [ "x$LAU_BUNDLE" = "x" ]; then
+	DIRNAME=`dirname "$0"`
+	LAU_BUNDLE="$DIRNAME/../"
+fi
+
+if [ ! -d "$LAU_BUNDLE" ]; then
+	echo "LAU_BUNDLE folder not exists! Check parameters!"
+	quit;
+fi
+
+
+
+if [ "x$LAU_HOME" = "x" ]; then
+	LAU_HOME="$WILDFLY_HOME/standalone/data/";
+fi
+
+#-------------------------------------------------------------------------------
+# copy library modules
+cp -r "$LAU_BUNDLE/modules/si" "$WILDFLY_HOME/modules/"
+cp "$LAU_BUNDLE/$APPL_SERVER/modules/si.laurentius.module.xml" "$WILDFLY_HOME/modules/si/laurentius/main/module.xml"
+
+
+# copy 'org' library modules (primefaces, pdfbox)
+cp -r "$LAU_BUNDLE/modules/org" "$WILDFLY_HOME/modules/"
+
+
+#-------------------------------------------------------------------------------
+# application modules
+cp "$LAU_BUNDLE/deployments/Laurentius-dao.jar"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/Laurentius-msh.ear"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/laurentius-ws.war"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/laurentius-web.war"  "$WILDFLY_HOME/standalone/deployments/"
+
+# application plugins
+cp "$LAU_BUNDLE/deployments/plugin-zpp.war"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/plugin-basic.war"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/plugin-testcase.war"  "$WILDFLY_HOME/standalone/deployments/"
+cp "$LAU_BUNDLE/deployments/example-web-plugin.war"  "$WILDFLY_HOME/standalone/deployments/"
+
+
+if [ "$INIT" = "TRUE" ]; then
+
+	# copy module fix
+	if [ -e "$LAU_BUNDLE/$APPL_SERVER/modules/org.apache.ws.security.module.xml" ]; then
+		cp "$LAU_BUNDLE/$APPL_SERVER/modules/org.apache.ws.security.module.xml" "$WILDFLY_HOME/modules/system/layers/base/org/apache/ws/security/main/module.xml"
+	fi
+
+	# copy 'org' library modules (primefaces, pdfbox)
+	cp -r "$LAU_BUNDLE/modules/org" "$WILDFLY_HOME/modules/"
+	
+
+	# copy start scripts
+	cp "$LAU_BUNDLE/$APPL_SERVER/laurentius-demo.sh" "$WILDFLY_HOME/bin/"
+	chmod u+x "$WILDFLY_HOME/bin/laurentius-demo.sh"
+
+	# create home folder
+	mkdir -p  "$LAU_HOME"
+	cp -r "$LAU_BUNDLE/laurentius-home" "$LAU_HOME"
+	
+	# copy configuration
+	cp "$LAU_BUNDLE/$APPL_SERVER/config/laurentius-roles.properties" "$WILDFLY_HOME/standalone/configuration/"
+	cp "$LAU_BUNDLE/$APPL_SERVER/config/laurentius-users.properties" "$WILDFLY_HOME/standalone/configuration/"
+	cp "$LAU_BUNDLE/$APPL_SERVER/config/standalone-laurentius.xml" "$WILDFLY_HOME/standalone/configuration/"
+	cp "$LAU_BUNDLE/$APPL_SERVER/config/test-tls-keystore.jks" "$WILDFLY_HOME/standalone/configuration/"
+
+	
+
+fi
+
