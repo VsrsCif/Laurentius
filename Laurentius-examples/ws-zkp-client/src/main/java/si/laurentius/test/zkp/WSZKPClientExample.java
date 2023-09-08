cfc3d9a2102705ecf329ae21f52a2497986e1f5d
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2023-09-08 11:05:52 +0200

    Revert ZKP test changes

--- Laurentius-examples/ws-zkp-client/src/main/java/si/laurentius/test/zkp/WSZKPClientExample.java
+++ Laurentius-examples/ws-zkp-client/src/main/java/si/laurentius/test/zkp/WSZKPClientExample.java
@@ -114,34 +114,34 @@ public class WSZKPClientExample {
          *
          */
         // ZKP_A
-//        try {
-//            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
-//            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_A_SERVICE, S_KEY_ALIAS, "Test message ZKP_A");
-//        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
-//            LOG.error("Signature failed: " + ex.getMessage(), ex);
-//        }
-//
-//        try {
-//            // 2. test: AdviceOfDelivery is signed with a key NOT registered in laurentius
-//            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_A_SERVICE, S_KEY_NOT_REGISTRED_ALIAS, "Test message ZKP_A");
-//        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
-//            LOG.error("Signature failed: " + ex.getMessage(), ex);
-//        }
+        try {
+            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
+            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_A_SERVICE, S_KEY_ALIAS, "Test message ZKP_A");
+        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
+            LOG.error("Signature failed: " + ex.getMessage(), ex);
+        }
+
+        try {
+            // 2. test: AdviceOfDelivery is signed with a key NOT registered in laurentius
+            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_A_SERVICE, S_KEY_NOT_REGISTRED_ALIAS, "Test message ZKP_A");
+        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
+            LOG.error("Signature failed: " + ex.getMessage(), ex);
+        }
 
         // ZKP_B
-//        try {
-//            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
-//            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_B_SERVICE, S_KEY_ALIAS, "Test message ZKP_B");
-//        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
-//            LOG.error("Signature failed: " + ex.getMessage(), ex);
-//        }
-//
-//        try {
-//            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
-//            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_B_SERVICE, S_KEY_NOT_REGISTRED_ALIAS, "Test message ZKP_B");
-//        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
-//            LOG.error("Signature failed: " + ex.getMessage(), ex);
-//        }
+        try {
+            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
+            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_B_SERVICE, S_KEY_ALIAS, "Test message ZKP_B");
+        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
+            LOG.error("Signature failed: " + ex.getMessage(), ex);
+        }
+
+        try {
+            // 1. test: AdviceOfDelivery is signed with key registered in laurentius
+            testAdviceOfDelivery(ZKPDeliveryConstants.S_ZKP_B_SERVICE, S_KEY_NOT_REGISTRED_ALIAS, "Test message ZKP_B");
+        } catch (SEDException_Exception | JAXBException | FOPException | ZKPException ex) {
+            LOG.error("Signature failed: " + ex.getMessage(), ex);
+        }
 
         // non-delivery for ZKP A and B
         try {
