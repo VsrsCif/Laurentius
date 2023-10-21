d44b8d116592d60e091856b662fa65b449b34efa
Gasper Vrhovsek <gasper.vrhovsek@gmail.com>
2023-10-21 17:16:01 +0200

    Add timestamp verification to timestampSignatureTest.timestampTest

--- Laurentius-plugins/Laurentius-eOdlozisce-plugin/src/test/java/si/vsrs/cif/laurentius/plugin/eodlozisce/tsa/TimestampSignatureTest.java
+++ Laurentius-plugins/Laurentius-eOdlozisce-plugin/src/test/java/si/vsrs/cif/laurentius/plugin/eodlozisce/tsa/TimestampSignatureTest.java
@@ -3,6 +3,7 @@ package si.vsrs.cif.laurentius.plugin.eodlozisce.tsa;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
+import si.src.setcce.sign.wsclient.VerifyResult;
 import si.vsrs.cif.laurentius.plugin.eodlozisce.sig.XMLSignatureUtils;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -14,8 +15,7 @@ public class TimestampSignatureTest {
 
     @Test
     public void timestampTest() throws ParserConfigurationException, IOException, SAXException, TransformerException, TimestampException {
-//        final String serverUrl = "http://raweb-test.sigov.si:8080/proxsignMJU_WS/setcceSignServerService";
-        final String serverUrl = "http://raweb.sigov.si:8080/proxsignMJU_WS/setcceSignServerService";
+        final String serverUrl = "http://raweb-test.sigov.si:8080/proxsignMJU_WS/setcceSignServerService";
         final String signatureNamespace = "http://www.w3.org/2000/09/xmldsig#";
         final int timeout = 8000;
         TimeStampServiceImpl tsaService = new TimeStampServiceImpl(serverUrl, signatureNamespace, timeout);
@@ -24,8 +24,13 @@ public class TimestampSignatureTest {
         Document doc = utils.parseDocument(Paths.get("src/test/resources/xml_sig_validation/signed.xml"));
         final String documentString = XMLSignatureUtils.documentToString(doc);
 
+        // Run timestamping ...
         final String timestampedXml = tsaService.timeStampXml(documentString);
-
         System.out.println(timestampedXml);
+
+        // Run verify timestamp
+        final VerifyResult verifyResult = tsaService.verifyTimestampSignature(timestampedXml);
+        System.out.printf("TSA verify result: resultCode: %s, errorMessage: %s, numberOfItems: %d%n",
+                verifyResult.getResultCode(), verifyResult.getErrMsg(), verifyResult.getNumberItems());
     }
 }
